fd56cc197136af2b09517c2332616085d4b531db
VATTI BHANU CHANDAR <bhanuchandar844@gmail.com>
2024-05-01 16:59:54 +0530

    bhanu

---
+++ index.js
@@ -0,0 +1,24 @@
+const text_box=document.getElementById("text_box");
+const fahrenheit=document.getElementById("fahrenheit");
+const celsius=document.getElementById("celsius");
+const result=document.getElementById("result");
+let temp;
+function convert(){
+    if(fahrenheit.checked){
+        temp=Number(text_box.value);
+        temp=temp*9/5+32;
+        result.textContent=temp.toFixed(1)+"°F";
+
+    }
+    else if(celsius.checked){
+        temp=Number(text_box.value);
+        temp=(temp-32)*5/9;
+        result.textContent=temp.toFixed(1)+"°C";
+
+
+    }
+    else{
+        result.textContent="Select  a unit";
+        
+    }
+}
